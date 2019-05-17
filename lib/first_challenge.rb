21976073db1b834d645000ae8f59467c462cff07
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-17 21:12:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,15 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.each do |flavor|
+          puts "#{flavor}"
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
